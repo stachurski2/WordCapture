d9eb85e6c4c62bf5b92aac04b93189dcdb5bf0bb
Stanislaw Sobczyk <stan@gofar.co>
2020-06-08 16:06:11 +0200

    Initial Commit

---
+++ SoundCapture/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SoundCapture
+//
+//  Created by Stanisaw Sobczyk on 08/06/2020.
+//  Copyright © 2020 Stanisaw Sobczyk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SoundCapture/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SoundCapture
+//
+//  Created by Stanisaw Sobczyk on 08/06/2020.
+//  Copyright © 2020 Stanisaw Sobczyk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
